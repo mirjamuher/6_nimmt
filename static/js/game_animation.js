d13ad5efb47ce779f10678b2bc0296e11a6b6f9c
Mirjam Uher <uher.mirjam@gmail.com>
2020-12-23 20:15:41 +1100

    first implementation: card animation and 'stack full' animation

--- static/js/game_animation.js
+++ static/js/game_animation.js
@@ -24,65 +24,52 @@
 // This one already has the right info in game notation (though I need to sort the players by the card thats played)
 // Find player through player ID
 
+const TIMER_MS = 3*1000;
+
 export function animatePlay(data) {
     console.log("I'm in animatePlay()")
-    for (let entry of data) {
-        console.log(`ROUND X - entry is ${entry}`);
-        // Get all important info and elements on page
-        const playerID = entry["player"]["player_id"]; // int of player ID
-        const elPlayer = document.querySelector(`[data-player-id="${playerID}"]`);
-        const crntStackIndex = entry["old_stack_index"];
-        const oldStack = entry["old_stack"];
-        const newStack = entry["new_stack"];
-        const playedCard = entry["played_card"];
-        console.log("PLAYED CARD IS");
-        console.log(playedCard);
-        const stackReplaced = entry["stack_replaced"];
+    // Call helper function to set timeout and give each round some breathing space
+    _animatePlay(data, 0);
+}
 
-        // highlight stack (aka row full of td elements) we are working on
-        const elHighlightedStack = highlightCrntStack(crntStackIndex);
-        console.log(elHighlightedStack);
-        elHighlightedStack.classList.add("highlighted");
+function _animatePlay(data, index) {
+    // Get all important info and elements on page
+    const entry = data[index];
+    const playerID = entry["player"]["player_id"]; // int of player ID
+    const elPlayer = document.querySelector(`#playerOverview [data-player-id="${playerID}"]`);
+    const crntStackIndex = entry["old_stack_index"];
+    const oldStack = entry["old_stack"];
+    const newStack = entry["new_stack"];
+    const playedCard = entry["played_card"];
+    console.log("PLAYED CARD IS");
+    console.log(playedCard);
+    const stackReplaced = entry["stack_replaced"];
 
-        // highlight current player
-        elPlayer.classList.add("highlighted");
+    // highlight stack (aka row full of td elements) we are working on
+    const elHighlightedStack = highlightCrntStack(crntStackIndex);
+    console.log(elHighlightedStack);
+    elHighlightedStack.classList.add("highlighted");
 
-        // If stack is not replaced, only new card is added -> make its own function?
-        if (!stackReplaced) {
-            console.log("Only new card needs adding");
+    // highlight current player
+    elPlayer.classList.add("highlighted");
 
-            // Pulls out each card in newStack and makes it a column in this row
-            for (let col=0; col<5; col++) {
-                const currentCard = newStack[col];
-                const elCurrentCell = elHighlightedStack.querySelector(`td:nth-child(${col+1})`);
+    // Deal with stack, if it is being replaced
+    if (stackReplaced) {
+        console.log("Stack needs replacement animation")
+        stackExit(elHighlightedStack);
+    }
+    // Add the new card by iterating through the stack
+    setTimeout(function() {addNewCard(newStack, playedCard, elHighlightedStack)}, 1*1000);
 
-                // sets cardValue to -1 for blanked out look, then overwrites if card exists
-                let currentCardValue = -1;
-                if (currentCard) {
-                    currentCardValue = currentCard["value"];
-                }
-
-                // Fit my-card into the right cell
-                const elNewCard = document.createElement("my-card");
-                elNewCard.cardValue = currentCardValue;
-                elNewCard.location = 'table';
-                elCurrentCell.innerHTML = "";
-                elCurrentCell.appendChild(elNewCard);
-
-                if (currentCardValue === playedCard["value"]) {
-                    elNewCard.classList.add("newCardAnimation"); //TODO: Add fading out animation in css
-                }
-            }
-        }
-
-        // Animation if stack is being replaced
-        if (stackReplaced) {
-            console.log("Stack needs replacement animation")
-        }
-
-        // end of loop cleanup
+    // end of loop cleanup, happens after a certain timeout
+    setTimeout(function() {
         elHighlightedStack.classList.remove("highlighted")
         elPlayer.classList.remove("highlighted");
+    }, TIMER_MS);
+
+    // If there is more data to go, call this function again with index incremented by 1
+    if (index+1 < data.length) {
+        setTimeout(function() {_animatePlay(data, index+1)}, TIMER_MS);
     }
 }
 
@@ -90,4 +77,45 @@ function highlightCrntStack(crntStackIndex) {
     const elStacks = document.querySelectorAll("#stacks tr"); // returns [tr, tr, tr, tr]
     const elHighlightedStack = elStacks[crntStackIndex];
     return elHighlightedStack;
+}
+
+function addNewCard(newStack, playedCard, elHighlightedStack) {
+    // Pulls out each card in newStack and makes it a column in this row
+    for (let col=0; col<5; col++) {
+        const currentCard = newStack[col];
+        const elCurrentCell = elHighlightedStack.querySelector(`td:nth-child(${col+1})`);
+
+        // sets cardValue to -1 for blanked out look, then overwrites if card exists
+        let currentCardValue = -1;
+        if (currentCard) {
+            currentCardValue = currentCard["value"];
+        }
+
+        // Fit my-card into the right cell
+        const elNewCard = document.createElement("my-card");
+        elNewCard.cardValue = currentCardValue;
+        elNewCard.location = 'table';
+        elCurrentCell.innerHTML = "";
+        elCurrentCell.appendChild(elNewCard);
+
+        if (currentCardValue === playedCard["value"]) {
+            elNewCard.classList.add("newCardAnimation"); //TODO: Add fading out animation in css
+        }
+
+        // Removes Animation-class after same timer as stack & player highlight
+        setTimeout(function() {
+            elNewCard.classList.remove("newCardAnimation");
+        }, TIMER_MS);
+    }
+}
+
+function stackExit(elHighlightedStack) {
+    // mark all cards in stack with new classname
+    elHighlightedStack.classList.add('animate__animated', 'animate__bounceOutLeft');
+    // TODO: get ou tof his file
+
+    // Remove Classlist after animation complete
+    elHighlightedStack.addEventListener('animationend', () => {
+        elHighlightedStack.classList.remove('animate__animated', 'animate__bounceOutLeft');
+    });
 }
